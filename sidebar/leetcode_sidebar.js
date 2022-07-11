4310f0eccfa93f105bd93ec5245942165a502b23
CliffHu <ayowi@CliffHudeAir.lan>
2022-07-11 16:36:27 +0800

    add link list / prettier some file

--- sidebar/leetcode_sidebar.js
+++ sidebar/leetcode_sidebar.js
@@ -9,6 +9,42 @@ const leetcode_sidebar = {
     },
     {
       type: "category",
+      label: "Array",
+      items: [
+        {
+          type: "doc",
+          label: "0053 Maximum Subarray",
+          id: "leetcode/Array/leetcode-0053",
+        },
+        {
+          type: "doc",
+          label: "0088 Merge Sorted Array",
+          id: "leetcode/Array/leetcode-0088",
+        },
+        {
+          type: "doc",
+          label: "0118 Pascal's Triangle",
+          id: "leetcode/Array/leetcode-0118",
+        },
+        {
+          type: "doc",
+          label: "0121 Roman to Integer",
+          id: "leetcode/Array/leetcode-0121",
+        },
+        {
+          type: "doc",
+          label: "0350 Intersection of Two Arrays II",
+          id: "leetcode/Array/leetcode-0350",
+        },
+        {
+          type: "doc",
+          label: "0566 Reshape the Matrix",
+          id: "leetcode/Array/leetcode-0566",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "Hash Table",
       items: [
         {
@@ -30,6 +66,27 @@ const leetcode_sidebar = {
           type: "doc",
           label: "2006 Count Number of Pairs With Absolute Difference K",
           id: "leetcode/Hash Table/leetcode-2006",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Link List",
+      items: [
+        {
+          type: "doc",
+          label: "0021 Merge Two Sorted Lists",
+          id: "leetcode/Link List/leetcode-0021",
+        },
+        {
+          type: "doc",
+          label: "0141 Linked List Cycle",
+          id: "leetcode/Link List/leetcode-0141",
+        },
+        {
+          type: "doc",
+          label: "0203 Remove Linked List Elements",
+          id: "leetcode/Link List/leetcode-0203",
         },
       ],
     },
